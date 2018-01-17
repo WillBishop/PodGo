0959a92bb8235018481b6519524b84db887c9839
Will Bishop <123icebuggy@gmx.com>
2018-01-17 21:51:34 +1030

    Initial Commit

---
+++ PodGo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PodGo
+//
+//  Created by Will Bishop on 17/1/18.
+//  Copyright © 2018 Will Bishop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
